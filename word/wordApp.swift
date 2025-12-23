cc1e95c3b08e760c6aebbc79cce2a4cbf62f77be
thatshinji <li2.y@foxmail.com>
2025-12-24 02:23:41 +0800

    Initial Commit

---
+++ word/wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordApp.swift
+//  word
+//
+//  Created by Li2 on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
